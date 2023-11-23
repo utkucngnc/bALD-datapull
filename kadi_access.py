71cd26a695f819bf413b5c701baeae74fa2a7366
utkucngnc <utkucngnc@gmail.com>
2023-11-23 12:55:15 +0100

    KadiManager added

--- kadi_access.py
+++ kadi_access.py
@@ -1,11 +1,57 @@
 from kadi_apy import KadiManager
+from kadi_apy.lib import resources
+from kadi_apy.lib.resources import records
+from kadi_apy.lib.resources.users import User
+import urllib.request
+import json
+import logging
+from tqdm import tqdm
+import io
 
-manager = KadiManager()
+from datetime import date
+from datetime import time
+from datetime import datetime
 
-record = manager.record(
-    id = 1074
-)
+from utils import *
+
+class KadiInstance():
+    def __init__(self):
+        self.manager = None
+
+        self.kadiUrl = ""
+        self.isCreatedFlag = True
+        self.isUserValidated = False
+
+    def login(self,hostUrl: str,userToken :str):
+        print('Establisihing connection to Kadi...')
+        self.manager = KadiManager(host=str(hostUrl), token=str(userToken), verify=False)
+        response = self.manager.make_request(endpoint="/users/me", method="get")
+        if response.status_code == 200:
+            self.isUserValidated = True
+            self.user = self.manager.pat_user
+            print('Connection established')
+        else:
+            print('Connection failed, please check your token and url')
+
+    def userDetails(self):
+        return [self.user.name,self.user.id]
+    
+    def getRecordsByTag(self, tag: int, save = True):
+        endpoint = '/records/' + str(tag) + '/files'
+        print(endpoint)
+        response = self.manager.make_request(endpoint=endpoint, method="get")
+        items = response.json()['items']
+        for _,item in enumerate(tqdm(items)):
+            download_url = item['_links']['download']
+            df = get_file_as_dataframe(self.manager,download_url)
+            save_excel('./data/',item['name'],df) if save else None
 
 
-
-
+if __name__ == '__main__':
+    k = KadiInstance()
+    cfg = get_config()
+    token = cfg.kadi.token_hash
+    host = cfg.kadi.main_url
+    k.login(host,token)
+    k.getRecordsByTag(1704)
+    #print(k.userDetails())
--- utils.py
+++ utils.py
@@ -1,5 +1,8 @@
 from omegaconf import OmegaConf
 import pandas as pd
+import json
+import io
+import os
 
 def get_config(config_path: str = "config.yaml"):
     config = OmegaConf.load(config_path)
@@ -12,3 +15,28 @@ def read_csv(path):
 def read_excel(path):
     data = pd.read_excel(path)
     return data
+
+def save_json(path: str, data: dict):
+    with open(path, 'w') as f:
+        json.dump(data, f, indent=4)
+
+def save_excel(path: str, filename: str, data: pd.DataFrame):
+    if not os.path.exists(path):
+        os.makedirs(path)
+    try:
+        data.to_excel(path + filename, index=False, engine='openpyxl')
+    except:
+        raise Exception('Error while saving data file')
+
+def read_response(response):
+    with io.BytesIO(response.content) as fh:
+        df = pd.io.excel.read_excel(fh, engine='openpyxl')
+    
+    return df
+
+def get_file_as_dataframe(manager, download_url: str):
+    try:
+        response = manager.make_request(endpoint=download_url, method="get")
+        return read_response(response)
+    except:
+        raise Exception('Error while downloading file, check KadiManager connection')
